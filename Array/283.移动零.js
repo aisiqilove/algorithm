25ae4e481ed9c9f25e280825ebd3b4faa6c06260
maxzzli <592941317@qq.com>
2023-07-05 16:31:41 +0800

    feat: 修改

--- Array/283.移动零.js
+++ Array/283.移动零.js
@@ -10,16 +10,38 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    let fast = 0
-    let slow = 0
+    // 双指针 换位
+    // let fast = 0
+    // let slow = 0
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i]) {
+    //         [nums[fast], nums[slow]] = [nums[slow], nums[fast]]
+    //         slow++
+    //     }
+    //     fast++
+    // }
+    // return nums
+
+    // 找到0 删除 末尾添加0
+    // let temp = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if(!nums[temp]) {
+    //         nums.splice(temp,1)
+    //         nums[nums.length] = 0
+    //     } else {
+    //         temp++
+    //     }
+    // }
     for (let i = 0; i < nums.length; i++) {
+        let j = 0
         if (nums[i]) {
-            [nums[fast], nums[slow]] = [nums[slow], nums[fast]]
-            slow++
+            nums[j] = nums[i]
+            if (i != j) {
+                nums[i] = 0
+            }
+            j++
         }
-        fast++
     }
-    return nums
 };
 // @lc code=end
 
